79a05e1612d1f38977dc9fe0d2b04609bde312d0
Brieana P <bpolkper@ucsc.edu>
2016-01-31 09:46:15 -0800

    Update Collections
    add in forms for lib

--- ratemyboss/lib/collections.js
+++ ratemyboss/lib/collections.js
@@ -1,5 +1,26 @@
-  survey = new Mongo.Collection("survey");
-  survey.attachSchema(new SimpleSchema({
+  evaluations = new Mongo.Collection("evaluations");
+  evaluations.attachSchema(new SimpleSchema({
+    question1: {
+      type: String,
+      label: "When asking to get a task done, does your boss tell you in a well fair and respectable manner?\n",
+      max: 500
+    },
+    question2: {
+       type: String,
+       label: "Do they respect everyone one in the work space?\n",
+       max: 500
+    },
+    answer: {
+      type: String,
+      label: "answer",
+      optional: true,
+      max: 1000
+    }
+  }));
+
+
+  feedback = new Mongo.Collection("feedback");
+  feedback.attachSchema(new SimpleSchema({
     question: {
       type: String,
       label: "question",
@@ -10,7 +31,8 @@
       label: "comment",
       optional: true,
       max: 1000
-    }
-  }));
+    }   
+   }));
+  
   usersId = new Mongo.Collection("usersId");
   Photos = new Mongo.Collection("photos");
